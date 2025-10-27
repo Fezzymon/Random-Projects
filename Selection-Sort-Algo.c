9529ec0870e3e8db86110a5852a8413fea38bb30
Fezzymon <156960669+Fezzymon@users.noreply.github.com>
2025-10-27 13:00:26 -0400

    Refactor selection sort and add array search feature

--- Selection-Sort-Algo.c
+++ Selection-Sort-Algo.c
@@ -1,26 +1,45 @@
 #include <stdio.h>
 
-// Function to perform selection sort
-void selectionSort(int arr[], int n) {
-    int i, j, minIndex, temp;
-
-    // Traverse through all array elements
-    for (i = 0; i < n - 1; i++) {
-        // Find the minimum element in remaining unsorted array
-        minIndex = i;
-        for (j = i + 1; j < n; j++) {
-            if (arr[j] < arr[minIndex]) {
-                minIndex = j;
-            }
-        }
-
-        // Swap the found minimum element with the first element
-        if (minIndex != i) {
-            temp = arr[i];
-            arr[i] = arr[minIndex];
-            arr[minIndex] = temp;
-        }
+void selectionSort(int arr[], int n);
+void printArray(int arr[], int size);
+void arraySearch(int arr[], int size);
+// Main function
+int main() {
+    int n;
+    printf("Enter the Size of the Array: ");
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        printf("#%d: ", i+1);
+        scanf("%d", &arr[i]);
     }
+
+    printf("Original array: ");
+    printArray(arr, n);
+
+    selectionSort(arr, n);
+
+    printf("Sorted array: ");
+    printArray(arr, n);
+
+    arraySearch(arr, n);
+    return 0;
+}
+
+// Function to perform selection sort
+void selectionSort(int arr[], int size) {
+   int min_pos;
+   for(int i=0;i<size-1;i++){
+       min_pos=i;
+       for(int j=i+1;j<size;j++){
+           if(arr[i]>arr[j])min_pos=j;
+       }
+       if(min_pos != i){
+           int temp = arr[i];
+           arr[i] = arr[min_pos];
+           arr[min_pos] = temp;
+       }
+   }
 }
 
 // Function to print the array
@@ -32,19 +51,18 @@ void printArray(int arr[], int size) {
     printf("\n");
 }
 
-// Main function
-int main() {
-    int arr[] = {64, 34, 25, 12, 22, 11, 90};
-    int n = sizeof(arr) / sizeof(arr[0]);
-
-    printf("Original array: ");
-    printArray(arr, n);
-
-    selectionSort(arr, n);
-
-    printf("Sorted array: ");
-    printArray(arr, n);
-
-    return 0;
+void arraySearch(int arr[], int size){
+    int search;
+    printf("What number are you searching for: ");
+    scanf("%d",&search);
+    for(int i=0;i<size;i++){
+        if(arr[i]==search){
+            printf("%d is located at Array[%d]",search,i);
+            break;
+        }
+        if(i==size-1)printf("Your search was not found...");
+    }
 }
+
+
 
